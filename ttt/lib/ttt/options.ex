d36ec6b6d6f0890f5d7644920deb8384cf208109
sarahabimay <sarahabimay@gmail.com>
2016-03-07 17:14:03 +0000

    Option function to lookup the board_size or game_type from option
    @maikon
    @jsuchy

--- ttt/lib/ttt/options.ex
+++ ttt/lib/ttt/options.ex
@@ -1,4 +1,5 @@
 defmodule TTT.Options do
+
   @board_sizes  %{3 => "3X3"}
   @game_types %{HVH => "Human VS Human"}
 
@@ -10,6 +11,23 @@ defmodule TTT.Options do
     Map.values(@game_types)
   end
 
+  def lookup_board_size(:invalid), do: :invalid
+
+  def lookup_board_size(index) do
+    validate_board_size_option(index)
+    |> lookup_option(@board_sizes)
+  end
+
+  def lookup_game_type(index) do
+    validate_game_type_option(index)
+    |> lookup_option(@game_types)
+  end
+
+  def lookup_option(:invalid, _), do: :invalid
+  def lookup_option(option_choice, options_list) do
+    Enum.at(Map.keys(options_list), option_choice - 1)
+  end
+
   def validate_board_size_option(option) do
     validate_option(@board_sizes, option)
   end
@@ -18,10 +36,10 @@ defmodule TTT.Options do
     validate_option(@game_types, option)
   end
 
-  def validate_option(options, option) do
+  defp validate_option(options, option) do
     number_of_options = Enum.count(options)
     case Integer.parse(option) do
-      {x, _} when x > 0 and x <= number_of_options -> option
+      {x, _} when x > 0 and x <= number_of_options -> x
       :error -> :invalid
       _ -> :invalid
     end
